c1374ed63470e009e8f2d1c1c9e66a1067993a63
Mikhail <nxt933@gmail.com>
2025-03-07 13:26:52 -0800

    proverka

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+  {
+    name: "Город",
+    link: "https://images.unsplash.com/photo-1740548786545-443986b49549?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzMXx8fGVufDB8fHx8fA%3D%3D"
+  },
+  {
+    name: "Омерика наверное",
+    link: "https://images.unsplash.com/photo-1741290723082-bd54c16a21a8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+  }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,40 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector('.places__list');
+const template = document.querySelector('#card-template');
+
+// Функция создания карточки
+function createCard(cardData) {
+    const templateClone = template.content.cloneNode(true);
+    const cardImage = templateClone.querySelector('.card__image');
+    const cardTitle = templateClone.querySelector('.card__title');
+    const deleteButton = templateClone.querySelector('.card__delete-button');
+
+    // Передаем данные в карточку
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+
+    // Удаление карточки
+    deleteButton.addEventListener('click', () => {
+        deleteButton.closest('.places__item').remove();
+    });
+
+    return templateClone;
+}
+
+// Отображаем стартовые карточки
+initialCards.forEach((card) => {
+    const newCard = createCard(card);
+    placesList.append(newCard);
+});
+
+// Добавление новой карточки
+const addBtn = document.querySelector('.profile__add-button');
+addBtn.addEventListener('click', () => {
+    const randomCard = initialCards[Math.floor(Math.random() * initialCards.length)];
+    const newCard = createCard(randomCard);
+    placesList.append(newCard);
+});
