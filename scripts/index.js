3b83945d152f65db87d854d7f28f50cc4248fd3b
Mikhail <nxt933@gmail.com>
2025-03-08 11:19:29 -0800

    proverka

--- scripts/index.js
+++ scripts/index.js
@@ -8,39 +8,44 @@
 
 // @todo: Вывести карточки на страницу
 
-const placesList = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 const template = document.querySelector('#card-template');
 
 // Функция создания карточки
 function createCard(cardData) {
     const templateClone = template.content.cloneNode(true);
-    const cardImage = templateClone.querySelector('.card__image');
-    const cardTitle = templateClone.querySelector('.card__title');
-    const deleteButton = templateClone.querySelector('.card__delete-button');
+    const cardElement = templateClone.querySelector('.places__item');
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
 
-    // Передаем данные в карточку
     cardImage.src = cardData.link;
     cardImage.alt = cardData.name;
     cardTitle.textContent = cardData.name;
 
-    // Удаление карточки
-    deleteButton.addEventListener('click', () => {
-        deleteButton.closest('.places__item').remove();
-    });
+    setupDeleteButton(cardElement); // Подключаем функцию удаления
 
-    return templateClone;
+    return cardElement;
 }
 
 // Отображаем стартовые карточки
 initialCards.forEach((card) => {
     const newCard = createCard(card);
-    placesList.append(newCard);
+    cardsContainer.append(newCard);
 });
 
+// уделние карточки
+function setupDeleteButton(cardElement) {
+        const deleteButton = cardElement.querySelector('.card__delete-button');
+        deleteButton.addEventListener('click', () => {
+        cardElement.remove();
+    });
+}
+
+
 // Добавление новой карточки
-const addBtn = document.querySelector('.profile__add-button');
-addBtn.addEventListener('click', () => {
-    const randomCard = initialCards[Math.floor(Math.random() * initialCards.length)];
-    const newCard = createCard(randomCard);
-    placesList.append(newCard);
-});
+// const addBtn = document.querySelector('.profile__add-button');
+// addBtn.addEventListener('click', () => {
+//     const randomCard = initialCards[Math.floor(Math.random() * initialCards.length)];
+//     const newCard = createCard(randomCard);
+//     cardsContainer.append(newCard);
+// });
